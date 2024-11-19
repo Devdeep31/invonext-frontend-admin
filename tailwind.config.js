9deaaa644f6af7d5e40b3c2018b350105f95c2c7
Devdeep31 <deepgovale31@gmail.com>
2024-11-19 23:49:02 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",
-    './node_modules/flowbite-react/**/*.js',
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
     "./node_modules/flowbite/**/*.js", // Include Flowbite in content
   ],
   theme: {
     extend: {},
   },
-  darkMode: 'class', 
+  darkMode: 'class',
   plugins: [
-    // require('flowbite/plugin')({
-    //   charts : true,
-    // }),
-    require('flowbite/plugin'),
-    
+    flowbitePlugin,
   ],
 };
